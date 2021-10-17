dd566cec8e1e56ba7e84180752096888936030c3
shiv2022 <88486015+shiv2022@users.noreply.github.com>
2021-10-17 15:10:44 +0530

    Update Sales.sql
    added create database statement and prefixed database name in front of tablename

--- MySQL Partitioning/Sales.sql
+++ MySQL Partitioning/Sales.sql
@@ -1,4 +1,6 @@
-CREATE TABLE Sales ( cust_id INT NOT NULL, name VARCHAR(40),   
+CREATE DATABASE test_partition;
+
+CREATE TABLE test_partition.sales ( cust_id INT NOT NULL, name VARCHAR(40),   
 store_id VARCHAR(20) NOT NULL, bill_no INT NOT NULL,   
 bill_date DATE PRIMARY KEY NOT NULL, amount DECIMAL(8,2) NOT NULL)   
 PARTITION BY RANGE (year(bill_date))(   
